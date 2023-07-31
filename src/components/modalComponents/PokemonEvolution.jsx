5711cec53a0b509a65f1cb7687e84556cd507087
Sedrick Reyes <sedrickreyes.dev@gmail.com>
2023-07-31 17:53:18 +0800

    fixed render on non evolving pokemon

--- src/components/modalComponents/PokemonEvolution.jsx
+++ src/components/modalComponents/PokemonEvolution.jsx
@@ -7,14 +7,18 @@ export default function PokemonEvolution({ evolution }) {
   return (
     <Suspense fallback={<Loader size={48} />}>
       <div className='flex justify-around '>
-        {data?.map(({ name, image }) => {
-          return (
-            <div key={name}>
-              <img className='w-36 aspect-square' src={image} alt='' />
-              <h1 className='font-bold text-center capitalize'>{name}</h1>
-            </div>
-          );
-        })}
+        {data?.length === 0 ? (
+          <h1 className='font-bold mt-16'>This pokemon does not evolve</h1>
+        ) : (
+          data?.map(({ name, image }) => {
+            return (
+              <div key={name}>
+                <img className='w-36 aspect-square' src={image} alt='' />
+                <h1 className='font-bold text-center capitalize'>{name}</h1>
+              </div>
+            );
+          })
+        )}
       </div>
     </Suspense>
   );
