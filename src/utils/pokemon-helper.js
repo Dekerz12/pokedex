69a540d9e1e394398ba4ddc862ed42327fa91b7d
Sedrick Reyes <sedrickreyes.dev@gmail.com>
2023-07-31 19:13:29 +0800

    updated evolution chain

--- src/utils/pokemon-helper.js
+++ src/utils/pokemon-helper.js
@@ -64,32 +64,30 @@ export const formatEvolution = (evolution) => {
   };
 
   const normalizeEvolutionChain = (evolution) => {
-    return evolution.reduce(
-      (acc, nextPokemon) => {
-        acc.push({
-          name: nextPokemon.species.name,
-          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${extractId(
-            nextPokemon.species.url
-          )}.png`,
-        });
-        if (nextPokemon.evolves_to.length) {
-          return nextPokemon.evolves_to.reduce((acc2, finalPokemon) => {
-            acc2 = {
-              name: finalPokemon.species.name,
-              image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${extractId(
-                finalPokemon.species.url
-              )}.png`,
-            };
-            acc.push(acc2);
+    return evolution.reduce((acc, nextPokemon) => {
+      acc.push(firstEvolution);
+      acc.push({
+        name: nextPokemon.species.name,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${extractId(
+          nextPokemon.species.url
+        )}.png`,
+      });
+      if (nextPokemon.evolves_to.length) {
+        return nextPokemon.evolves_to.reduce((acc2, finalPokemon) => {
+          acc2 = {
+            name: finalPokemon.species.name,
+            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${extractId(
+              finalPokemon.species.url
+            )}.png`,
+          };
+          acc.push(acc2);
 
-            return acc;
-          }, {});
-        }
+          return acc;
+        }, {});
+      }
 
-        return acc;
-      },
-      [firstEvolution]
-    );
+      return acc;
+    }, []);
   };
 
   const evolutionChain = normalizeEvolutionChain(evolution.evolves_to);
